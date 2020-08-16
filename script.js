f5c3a596ba1697057b3836d34e0d633738361db6
Eric D. Torres <etorresnotary@gmail.com>
2020-08-16 00:16:40 -0500

    giving up on jQuery to dynamically add slots

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-$("<div></div>").insertAfter(".row").addClass("row");
-$("<div></div>").appendTo(".row").last(".row").addClass("w-25 text-right hour");
-$("<div></div>").appendTo(".row").last(".row").addClass("w-50 text-center description");
-$("<div></div>").appendTo(".row").last(".row").addClass("w-25 text-left saveBtn");
+// $("<div></div>").insertAfter(".row").addClass("row");
+// $("<div></div>").appendTo(".row").last(".row").addClass("w-25 text-right hour");
+// $("<div></div>").appendTo(".row").last(".row").addClass("w-50 text-center description");
+// $("<div></div>").appendTo(".row").last(".row").addClass("w-25 text-left saveBtn");
+// let rowEl = $(".row");
+// let hourEl = $(".w-25 text-right hour");
+// let descriptionEl = $(".w-50 text-center description");
+// let saveBtn = $(".w-25 text-left saveBtn");
+
+
+// for (var i = 0; i < 17; i++) {
+//     rowEl.after("<div class='row'></div>");
+// }
+
+// $("<div></div>").appendTo(".row:not(:first)").addClass("w-25 text-right hour");
+// $("<div><textarea/></div>").appendTo(".row:not(:first)").addClass("w-50 text-center description");
+// $("<div></div>").appendTo(".row:not(:first)").addClass("w-25 text-left saveBtn");
+
+// function addTime() {
+//     $(".w-25 text-right hour").text("9:00 AM")
+// };
+
+// addTime();
