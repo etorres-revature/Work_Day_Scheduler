04d7660268da4ec7ea37d7505d8603b55f5e6a22
Eric D. Torres <etorresnotary@gmail.com>
2020-08-15 20:13:51 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+$("<div></div>").insertAfter(".row").addClass("row");
+$("<div></div>").appendTo(".row").last(".row").addClass("w-25 text-right hour");
+$("<div></div>").appendTo(".row").last(".row").addClass("w-50 text-center description");
+$("<div></div>").appendTo(".row").last(".row").addClass("w-25 text-left saveBtn");
