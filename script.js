b023d3c7fe66e2d94e437f9ccbaf22b68c3f6746
Eric D. Torres <etorresnotary@gmail.com>
2020-08-16 10:31:38 -0500

    background working

--- script.js
+++ script.js
@@ -6,17 +6,18 @@ $("#currentDay").text(today.format("[Today's Date:] ddd; MMMM Do, YYYY"));
 $("#timeOfDay").text(time);
 
 let currentHour = moment().format("H");
+console.log(currentHour);
 
 for (let i = 0; i <= 23; i++) {
     newBackground = i;
-    if (currentHour === i) {
+    if (currentHour == i) {
         $("#" + newBackground).addClass("present");
-        $("#"+ newBackground).sibling("div").children("textarea").addClass("present");
+        $("#"+ newBackground).siblings("div").children("textarea").addClass("present");
     } else if (currentHour > i) {
         $("#" + newBackground).addClass("past");
-        $("#" + newBackground).sibling("div").children("textarea").addClass("past");
+        $("#" + newBackground).siblings("div").children("textarea").addClass("past");
     } else {
         $("#" + newBackground).addClass("future");
-        $("#" + newBackground).sibling("div").childre("textarea").addClass("future")
+        $("#" + newBackground).siblings("div").children("textarea").addClass("future")
     }
 }
